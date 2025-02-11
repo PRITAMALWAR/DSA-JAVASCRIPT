5acf619b585555ebcbde5a1cd9764c778a7dd2d0
Pritam <pritamkumar08102004@gmail.com>
2025-02-11 16:24:11 +0530

    add new problem

---
+++ Practice/Problem03.js
@@ -0,0 +1,26 @@
+
+
+// let str = "masaischool"
+// let bag = ""
+// for(let i = 0; i<str.length; i++){
+//     bag += str[i] + " "
+// }
+// console.log(bag)
+
+
+
+// count the words in string
+
+
+let str = "hello pritam verma"
+
+let count = 0;
+
+for(let i = 0; i<str.length; i++){
+
+    if(str[i] == " "){
+        count++
+    }
+  
+}
+console.log(count + 1)
