7370a954632b90937e29ed152ae8878ad83fe557
Pritam <pritamkumar08102004@gmail.com>
2025-02-11 16:05:28 +0530

    add new problem

--- Practice/Problem01.js
+++ Practice/Problem01.js
@@ -1,8 +1,16 @@
 
-let sum = 0;
+// let sum = 0;
 
-for(let i = 0; i<=10; i++){
-    sum+= i
+// for(let i = 0; i<=10; i++){
+//     sum+= i
     
-}
-console.log(sum)
+// }
+// console.log(sum)
+
+
+
+
+
+// let numbers = [29,23,45,1, 2, 3];
+// console.log(Math.max(...numbers)); 
+// console.log(Math.min(...numbers)); 
